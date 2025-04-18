6b3015b9bdcd505e436d2b611677fb04ef1e2d6b
Andres Maneiro <amaneiro7@gmail.com>
2025-04-17 20:00:55 -0400

    ccimmit this chnagefd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,32 @@
+
 // @ts-check
 
 import eslint from '@eslint/js'
 import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  prettierConfig
-)
+export default {
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    pluginQuery,
+    reactPlugin,
+    reactHooksPlugin
+  ],
+  extends: [
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    prettierConfig,
+    'plugin:@tanstack/query/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
--- src/api/api.ts
+++ src/api/api.ts
@@ -5,35 +5,43 @@ import { api } from './axios.config'
 export async function fetching<T>(config: AxiosRequestConfig & { _retry?: boolean }): Promise<T> {
 	try {
 		const response = await api(config)
-		if (!response.data) throw new Error('Ha ocurrido un error inesperado')
+		if (!response.data) {
+			throw new Error('Ha ocurrido un error inesperado')
+		}
 		return response.data as T
 	} catch (error) {
-		if (mode === 'development') {
-			if (axios.isAxiosError(error) && error.response) {
-				throw new Error(error.response.data || 'Error desconocido')
-			} else if (error instanceof Error) {
-				throw new Error('No se ha podido realizar la petición')
-			} else {
-				throw new Error('No se ha podido realizar la petición')
-			}
-		}
-		if (axios.isAxiosError(error) && error.response) {
-			const { status } = error.response
-			if (status === 401) {
-				throw new Error('No autorizado')
-			}
-			if (status === 403) {
-				throw new Error('Acceso Denegado')
-			}
-			if (status === 404) {
-				throw new Error('No encontrado')
-			}
-			if (status === 500) {
-				throw new Error('Error interno del servidor')
-			}
+		const axiosError = axios.isAxiosError(error)
 
-			throw new Error(error.response.data || 'Error desconocido')
+		if (mode === 'development') {
+			if (axiosError && error.response) {
+				throw new Error(
+					error.response.data || `Error HTTP ${error.response.status}: Error desconocido`
+				)
+			} else if (error instanceof Error) {
+				throw new Error(`Error de petición: ${error.message}`)
+			} else {
+				throw new Error('Error desconocido en la petición.')
+			}
 		}
-		throw new Error('Ha ocurrido un error. Por favor, inténtelo de nuevo más tarde')
+
+		if (axiosError && error.response) {
+			const { status, data } = error.response
+
+			switch (status) {
+				case 401:
+					throw new Error('No autorizado.')
+				case 403:
+					throw new Error('Acceso denegado.')
+				case 404:
+					throw new Error('Recurso no encontrado.')
+				case 500:
+					throw new Error('Error interno del servidor.')
+				default:
+					throw new Error(data || `Error HTTP ${status}: Error desconocido`)
+			}
+		}
+		throw new Error(
+			'Ha ocurrido un error al realizar la petición. Por favor, inténtelo de nuevo más tarde.'
+		)
 	}
 }
