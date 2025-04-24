e7fd62d71af8ac0d9af4ca670306718d50290ba9
Andres Maneiro <amaneiro7@gmail.com>
2025-04-24 15:40:47 -0400

    elimnar etiqueta que ocultaba el menu

--- src/components/Layout/HamburgerMenu.tsx
+++ src/components/Layout/HamburgerMenu.tsx
@@ -10,14 +10,13 @@ export const HamburgerMenu = memo(() => {
 		<>
 			<input type="checkbox" id="nav-toggle" aria-hidden hidden ref={navToggleRef} />
 			<label
-				className="hamburger hamburger--spin sr-only"
+				className="hamburger hamburger--spin"
 				htmlFor="nav-toggle"
 				aria-haspopup
 				aria-expanded={false}
 				aria-label="Menú de navegación"
 				aria-controls="main-navigation"
 			>
-				Menú de navegación
 				<span className="hamburger-box">
 					<span className="hamburger-inner bg-white before:bg-white after:bg-white" />
 				</span>
