e40d0012d91d3672dfe8d5d224bd41ff6af4d64b
Andres Maneiro <amaneiro7@gmail.com>
2025-04-28 02:08:41 -0400

    se agrego el el componente seo y optimizo la carga del font

---
+++ src/components/Seo.tsx
@@ -0,0 +1,12 @@
+interface SeoProps {
+	title: string
+	description: string
+}
+export function Seo({ title, description }: SeoProps) {
+	return (
+		<>
+			<title>{title}</title>
+			<meta name="description" content={description} />
+		</>
+	)
+}
