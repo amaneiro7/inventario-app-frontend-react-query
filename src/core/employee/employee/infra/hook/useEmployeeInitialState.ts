212ed34496afda1d4dbd3916dbf527091a2f97d9
Andres Maneiro <amaneiro7@gmail.com>
2025-04-30 01:51:45 -0400

    se manejo el error en el caso de phone o extension es null

--- src/core/employee/employee/infra/hook/useEmployeeInitialState.ts
+++ src/core/employee/employee/infra/hook/useEmployeeInitialState.ts
@@ -33,48 +33,55 @@ export function useEmployeeInitialState(defaultState: DefaultEmployee): {
 		retry: false
 	})
 
-	const mappedEmployeeState = useCallback((employee: EmployeeDto): void => {
-		setState({
-			id: employee.id,
-			userName: employee.userName,
-			type: employee.type,
-			name: employee.name ?? '',
-			lastName: employee.lastName ?? '',
-			email: employee.email ?? '',
-			isStillWorking: employee.isStillWorking,
-			employeeCode: employee.employeeCode ?? '',
-			nationality: employee.nationality ?? '',
-			cedula: employee.cedula ?? '',
-			locationId: employee.locationId ?? '',
-			directivaId: employee.directivaId ?? '',
-			vicepresidenciaEjecutivaId: employee.vicepresidenciaEjecutivaId ?? '',
-			vicepresidenciaId: employee.vicepresidenciaId ?? '',
-			departamentoId: employee.departamentoId ?? '',
-			cargoId: employee.cargoId ?? '',
-			extension: employee.extension,
-			phone: employee.phone,
-			extensionSegments: employee.extension.map(extension => {
-				const match = extension.match(/(\d{4})(\d{7})/)
-				const operadora = match ? match?.[1] : ''
-				const numero = match ? match?.[2] : ''
+	const mappedEmployeeState = useCallback(
+		(employee: EmployeeDto): void => {
+			setState(() => {
+				const phone = employee.phone ?? []
+				const extension = employee.extension ?? []
+				return {
+					id: employee.id,
+					userName: employee.userName,
+					type: employee.type,
+					name: employee.name ?? '',
+					lastName: employee.lastName ?? '',
+					email: employee.email ?? '',
+					isStillWorking: employee.isStillWorking,
+					employeeCode: employee.employeeCode ?? '',
+					nationality: employee.nationality ?? '',
+					cedula: employee.cedula ?? '',
+					locationId: employee.locationId ?? '',
+					directivaId: employee.directivaId ?? '',
+					vicepresidenciaEjecutivaId: employee.vicepresidenciaEjecutivaId ?? '',
+					vicepresidenciaId: employee.vicepresidenciaId ?? '',
+					departamentoId: employee.departamentoId ?? '',
+					cargoId: employee.cargoId ?? '',
+					extension: extension,
+					phone: phone,
+					extensionSegments: extension?.map(extension => {
+						const match = extension.match(/(\d{4})(\d{7})/)
+						const operadora = match ? match?.[1] : ''
+						const numero = match ? match?.[2] : ''
 
-				return { operadora, numero }
-			}) ?? [
-				{
-					operadora: '',
-					numero: ''
+						return { operadora, numero }
+					}) ?? [
+						{
+							operadora: '',
+							numero: ''
+						}
+					],
+					phoneSegments: phone.map(phone => {
+						const match = phone.match(/(\d{4})(\d{7})/)
+						const operadora = match ? match?.[1] : ''
+						const numero = match ? match?.[2] : ''
+						return { operadora, numero }
+					}),
+					devices: employee.devices ?? [],
+					updatedAt: employee.updatedAt
 				}
-			],
-			phoneSegments: employee.phone.map(phone => {
-				const match = phone.match(/(\d{4})(\d{7})/)
-				const operadora = match ? match?.[1] : ''
-				const numero = match ? match?.[2] : ''
-				return { operadora, numero }
-			}),
-			devices: employee.devices ?? [],
-			updatedAt: employee.updatedAt
-		})
-	}, [])
+			})
+		},
+		[setState]
+	)
 
 	useEffect(() => {
 		if (mode === 'add' || !location.pathname.includes('employee')) {
