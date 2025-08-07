135aa66f50e5df76decb6b9aa850ee38dfe848b2
Andres Maneiro <amaneiro7@gmail.com>
2025-08-07 05:26:12 -0400

    se agrego por separado el classname ya que lo estaba sobrescribiendo en vez de solo adjuntar

--- src/shared/ui/DetailsWrapper/DetailsBoxWrapper.tsx
+++ src/shared/ui/DetailsWrapper/DetailsBoxWrapper.tsx
@@ -11,17 +11,19 @@ interface Props
 
 type Position = 'left' | 'center' | 'right'
 
-export const DetailsBoxWrapper = memo(({ position = 'left', children, ...props }: Props) => {
-	const classes = twMerge(
-		'w-full p-4 flex flex-col gap-3 bg-white shadow-sm rounded-2xl',
-		cn({
-			[`items-${position}`]: position
-		}),
-		props.className
-	)
-	return (
-		<div className={classes} {...props}>
-			{children}
-		</div>
-	)
-})
+export const DetailsBoxWrapper = memo(
+	({ position = 'left', children, className, ...props }: Props) => {
+		const classes = twMerge(
+			'w-full p-4 flex flex-col gap-3 bg-white shadow-sm rounded-2xl',
+			cn({
+				[`items-${position}`]: position
+			}),
+			className
+		)
+		return (
+			<div className={classes} {...props}>
+				{children}
+			</div>
+		)
+	}
+)
