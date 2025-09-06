e5ab15ee88f8a4470fca5f4d7e26127a09b1abb9
Andres Maneiro <amaneiro7@gmail.com>
2025-09-05 23:53:37 -0400

    se modifico elbutton para que permite tener un texto vacio pero con accesibilidad

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react'
-import cn from 'classnames'
-import { twMerge } from 'tailwind-merge'
+import { Color, Size, ButtonSize } from './styles'
+import { cn } from '@/shared/lib/utils'
 
 interface Props
 	extends React.DetailedHTMLProps<
@@ -8,6 +8,7 @@ interface Props
 		HTMLButtonElement
 	> {
 	text: string
+	srOnly?: boolean
 	color: keyof typeof Color
 	size: keyof typeof Size
 	buttonSize: keyof typeof ButtonSize
@@ -15,8 +16,6 @@ interface Props
 	hoverTranslation?: boolean
 	icon?: React.JSX.Element | null
 }
-
-import { Color, Size, ButtonSize } from './styles'
 
 const hoverStyle = 'hover:shadow-lg disabled:translate-y-0 hover:-translate-y-1'
 
@@ -28,22 +27,27 @@ function Button({
 	buttonSize,
 	size,
 	color,
+	srOnly = false,
 	...props
 }: Props) {
-	const classes = twMerge(
-		'flex justify-center text-b items-center gap-2 font-medium rounded-md cursor-pointer border border-solid transition-all duration-200 ease-in disabled:opacity-70 disabled:cursor-not-allowed',
-		cn({
-			[`${Color[color]}`]: color,
-			[`${ButtonSize[buttonSize]}`]: buttonSize,
-			[`${Size[size]}`]: size,
-			[hoverStyle]: hoverTranslation
-		}),
-		className
-	)
 	return (
-		<button className={classes} aria-label={`${text}`} title={`${text}`} {...props}>
+		<button
+			className={cn(
+				'text-b flex cursor-pointer items-center justify-center gap-2 rounded-md border border-solid font-medium transition-all duration-200 ease-in disabled:cursor-not-allowed disabled:opacity-70',
+				{
+					[`${Color[color]}`]: color,
+					[`${ButtonSize[buttonSize]}`]: buttonSize,
+					[`${Size[size]}`]: size,
+					[hoverStyle]: hoverTranslation
+				},
+				className
+			)}
+			aria-label={`${text}`}
+			title={`${text}`}
+			{...props}
+		>
 			{icon}
-			{text}
+			<span className={cn(srOnly && 'sr-only')}>{text}</span>
 		</button>
 	)
 }
