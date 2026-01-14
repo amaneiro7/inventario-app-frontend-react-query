97548e3d2b249121fa4d8360908e638fe6660768
Andres Maneiro <amaneiro7@gmail.com>
2026-01-14 14:58:19 -0400

    link para abrir portal de impresoras

--- src/entities/devices/devices/infra/ui/DeviceTable/PrinterInformation.tsx
+++ src/entities/devices/devices/infra/ui/DeviceTable/PrinterInformation.tsx
@@ -1,3 +1,4 @@
+import { ExternalLink } from 'lucide-react'
 import { CardDetail } from '@/shared/ui/DescriptionList/CardDetail'
 import { DetailItem } from '@/shared/ui/DescriptionList/DetailItem'
 import { Icon } from '@/shared/ui/icon/Icon'
@@ -6,7 +7,23 @@ import { type DeviceDto } from '../../../domain/dto/Device.dto'
 export const PrinterInformation = ({ device }: { device: DeviceDto }) => {
 	const { printer, model } = device
 	return (
-		<CardDetail title="Especificaciones de la Impresora" icon={<Icon name="printer" />}>
+		<CardDetail
+			title="Especificaciones de la Impresora"
+			icon={<Icon name="printer" />}
+			headerAction={
+				printer?.ipAddress && (
+					<a
+						className="flex items-center gap-2 rounded-lg bg-blue-100 px-3 py-1 text-xs font-medium text-blue-800 transition-colors hover:bg-blue-200"
+						href={`http://${printer?.ipAddress}`}
+						rel="noreferrer"
+						target="_blank"
+					>
+						<ExternalLink className="h-3 w-3" />
+						Abrir Panel
+					</a>
+				)
+			}
+		>
 			{printer?.ipAddress && (
 				<DetailItem
 					classNameText="font-mono"
