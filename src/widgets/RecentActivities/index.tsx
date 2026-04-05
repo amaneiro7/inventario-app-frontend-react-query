4cde754f1e44b9769ae65269168520fde4033dfa
Andres Maneiro <amaneiro7@gmail.com>
2026-04-05 02:08:54 -0400

    se actulizo un componente de histroy

--- src/widgets/RecentActivities/index.tsx
+++ src/widgets/RecentActivities/index.tsx
@@ -2,43 +2,9 @@ import { memo } from 'react'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/shared/ui/Card'
 import { useGetAllHistorys } from '@/entities/history/infra/hook/useGetAllHistory'
 import { getRelativeTime } from '@/shared/lib/utils/getRelativeTime'
-import {
-	Laptop,
-	Monitor,
-	ScreenShareIcon,
-	Computer,
-	Server,
-	PrinterCheck,
-	Keyboard,
-	HardDrive,
-	Mouse,
-	Usb,
-	ScanIcon,
-	Phone,
-	Mic,
-	HdmiPort,
-	Tablet,
-	Camera,
-	Webcam,
-	Pencil,
-	Dock,
-	Speaker
-} from 'lucide-react'
-import { CategoryOptions } from '@/entities/category/domain/entity/CategoryOptions'
 import Typography from '@/shared/ui/Typography'
-
-const getActivityColor = (action: string) => {
-	switch (action) {
-		case 'CREATE':
-			return 'text-naranja-500 bg-naranja-100'
-		case 'UPDATE':
-			return 'text-verde-500 bg-verde-100'
-		case 'DELETE':
-			return 'text-rojo-500 bg-rojo-100'
-		default:
-			return 'text-slate-500 bg-slate-100'
-	}
-}
+import { getHistoryActionClassName } from '@/entities/history/infra/ui/getHistoryActionClassName'
+import { GetDeviceIcon } from '@/entities/category/infra/ui/GetDeviceIcon'
 
 export const RecentActivities = memo(() => {
 	const { data: histories, isLoading } = useGetAllHistorys({ pageSize: 5 })
@@ -54,7 +20,6 @@ export const RecentActivities = memo(() => {
 			<CardContent className="p-0">
 				<ul className="divide-y">
 					{histories.data.map(history => {
-						const Icon = getCategoryIcon(history.device.categoryId)
 						const action =
 							history.action === 'CREATE'
 								? 'Agregado'
@@ -68,9 +33,15 @@ export const RecentActivities = memo(() => {
 							>
 								<div className="flex items-start gap-4">
 									<div
-										className={`rounded-full p-2 ${getActivityColor(history.action)}`}
+										className={`rounded-full p-2 ${getHistoryActionClassName(history.action)}`}
 									>
-										{Icon && <Icon width={20} height={20} />}
+										{
+											<GetDeviceIcon
+												categoryId={history.device?.categoryId}
+												width={20}
+												height={20}
+											/>
+										}
 									</div>
 									<div className="min-w-0 flex-1">
 										<p className="text-sm font-medium text-slate-900">
@@ -92,42 +63,3 @@ export const RecentActivities = memo(() => {
 		</Card>
 	)
 })
-
-// Objeto que mapea categoryId a un componente de icono de Lucide React
-const categoryIconMap: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
-	[CategoryOptions.COMPUTER]: Computer,
-	[CategoryOptions.SERVER]: Server,
-	[CategoryOptions.LAPTOP]: Laptop,
-	[CategoryOptions.ALLINONE]: ScreenShareIcon,
-	[CategoryOptions.MONITOR]: Monitor,
-	[CategoryOptions.FINANTIALPRINTER]: PrinterCheck,
-	[CategoryOptions.LASERPRINTER]: PrinterCheck,
-	[CategoryOptions.INKPRINTER]: PrinterCheck,
-	[CategoryOptions.HARDDRIVE]: HardDrive,
-	[CategoryOptions.KEYBOARD]: Keyboard,
-	[CategoryOptions.MOUSE]: Mouse,
-	[CategoryOptions.BAM]: Monitor,
-	[CategoryOptions.MFP]: PrinterCheck,
-	[CategoryOptions.PHONE]: Phone,
-	[CategoryOptions.SCANNER]: ScanIcon,
-	[CategoryOptions.ANTENAS]: Monitor,
-	[CategoryOptions.CABLEUSB]: Usb,
-	[CategoryOptions.CAMARAS]: Camera,
-	[CategoryOptions.IPAD]: Tablet,
-	[CategoryOptions.WEBCAM]: Webcam,
-	[CategoryOptions.CORNETAS]: Speaker,
-	[CategoryOptions.DOCKING]: Dock,
-	[CategoryOptions.LAPIZOPTICO]: Pencil,
-	[CategoryOptions.CONVERTIDORVGAHDMI]: HdmiPort,
-	[CategoryOptions.MIC]: Mic
-}
-
-// Función para obtener el componente de icono basado en el categoryId
-const getCategoryIcon = (
-	categoryId: string | undefined
-): React.FC<React.SVGProps<SVGSVGElement>> => {
-	if (!categoryId) {
-		return Computer // O podrías devolver un icono por defecto
-	}
-	return categoryIconMap[categoryId] ?? Computer
-}
